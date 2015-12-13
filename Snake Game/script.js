cba1adcd243d8d90a185e5f364259659548c9c98
raitraak <raitraak@khk.ee>
2015-12-13 16:13:29 +0200

    Lecture 26: Snake Game Scripting 26 B [Delivers #110028634]

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,53 +1,116 @@
-$(document).ready(function() {
-    //Define vars
-
-    var canvas = $('#canvas')[0];
-    var ctx = canvas.getContext('2d');
-    var w = canvas.width();
-    var h = canvas.height();
+$(document).ready(function(){
+    //Define Vars
+    var canvas = $("#canvas")[0];
+    var ctx = canvas.getContext("2d");
+    var w = $("#canvas").width();
+    var h = $("#canvas").height();
     var cw = 15;
-    var d = 'right';
+    var d = "right";
     var food;
     var score;
-    var speed = 130;
+    var color ="blue";
+    var speed = 100;
 
     //Snake Array
-
     var snake_array;
 
-    function init() {
-
+    //Initializer
+    function init(){
+        d = "right";
         create_snake();
         create_food();
         score = 0;
 
-        if(typeof) game_loop !='undefined') clearInterval(game_loop);
+        //Use a timer to call paint function
+        if(typeof game_loop != "undefined") clearInterval(game_loop);
         game_loop = setInterval(paint, speed);
-
-
     }
 
-    init(); //run initializer
+    init();
 
-    //snake create
-
-    function create_snake() {
-        var lenght = 5;
+    //Create Snake
+    function create_snake(){
+        var length = 5;
         snake_array = [];
-
-        for(var i = lenght-1;i >= 0;i ==) {
-            snake_array.push({x: i,y:0});
+        for(var i = length-1; i>=0; i--){
+            snake_array.push({x: i, y:0});
         }
     }
 
-    //create food
-
-    function create_food() {
+    //Create Food
+    function create_food(){
         food = {
-            x:Math.round(Math.random()*(w-cw)/cw),
-            y:Math.round(Math.random()*(h-cw)/cw),
-        }
+            x: Math.round(Math.random()*(w-cw)/cw),
+            y: Math.round(Math.random()*(h-cw)/cw),
+        };
     }
 
+    //Paint Snake
+    function paint(){
+        //Paint The Canvas
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, w, h);
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(0, 0, w, h);
+
+        var nx = snake_array[0].x;
+        var ny = snake_array[0].y;
+
+        if(d == "right") nx++;
+        else if(d == "left") nx--;
+        else if(d == "up") ny--;
+        else if(d == "down") ny++;
+
+        //Collide code
+        if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array)){
+            //init();
+            //Insert Final Score
+            $('#final_score').html(score);
+            //Show Overlay
+            $('#overlay').fadeIn(300);
+            return;
+        }
+
+        if(nx == food.x && ny == food.y){
+            var tail = {x: nx, y: ny};
+            score++;
+            //Create Food
+            create_food();
+        } else {
+            var tail = snake_array.pop();
+            tail.x = nx; tail.y = ny;
+        }
+
+        snake_array.unshift(tail);
+
+        for(var i = 0;i < snake_array.length;i++){
+            var c = snake_array[i];
+            paint_cell(c.x,c.y);
+        }
+
+        //Paint Cell
+        paint_cell(food.x,food.y);
+
+        //Check Score
+        checkscore(score);
+
+        //Display Current Score
+        $('#score').html('Your Score: '+score);
+    }
+
+    function paint_cell(x,y){
+        ctx.fillStyle=color;
+        ctx.fillRect(x*cw,y*cw,cw,cw);
+        ctx.strokeStyle="white";
+        ctx.strokeRect(x*cw,y*cw,cw,cw);
+    }
+
+    function check_collision(x, y, array){
+        for(var i = 0;i < array.length;i++){
+            if(array[i.x == x && array[i].y ==y])
+                return true;
+        }
+        return false;
+    }
 
 });
