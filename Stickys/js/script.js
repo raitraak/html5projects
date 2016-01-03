b17c2ac763c401481ee58447a4838253a74dc9f0
raitraak <raitraak@khk.ee>
2016-01-03 18:13:25 +0200

    Lecture 35: Saving Notes & Debugging A [Delivers #110921762]

--- Stickys/js/script.js
+++ Stickys/js/script.js
@@ -1,115 +1,77 @@
 var db = null;
 
-if(window.openDatabase) {
-
-    db = openDatabase('NoteTest','1.0','Stickys Database','1000000');
-    if(!db) {
-        alert('Failed to open database');
+if(window.openDatabase){
+    db = openDatabase("NoteTest","1.0","Stickys Database",10000000);
+    if(!db){
+        alert("Failed to open database");
     }
-
 } else {
-    alert('Failed to open database, make sure your browser supports HTML5 Web storage');
-
+    alert("Failed to open database, make sure your browser supports HTML5 web storage");
 }
 
 var captured = null;
 var highestZ = 0;
 var highestId = 0;
 
-function Note() {
+function Note(){
     var self = this;
 
     var note = document.createElement('div');
     note.className = 'note';
-    note.addEventListener('mousedown', function(e) {
-        return self.onMousedown(e)
-
-    }, false);
-
-    note.addEventListener('click',function(e) {
-
-            return self.onNoteClick()
-
-        },false
-    );
-
+    note.addEventListener('mousedown', function(e) { return self.onMouseDown(e) }, false);
+    note.addEventListener('click', function() { return self.onNoteClick() }, false);
     this.note = note;
 
     var close = document.createElement('div');
     close.className = 'closebutton';
-
-    close.addEventListener('click',function(e) {
-
-            return self.close(e)
-
-        },false
-    );
-
+    close.addEventListener('click', function(event) { return self.close(event) }, false);
     note.appendChild(close);
 
-    var edit = createElement('div');
+    var edit = document.createElement('div');
     edit.className = 'edit';
-    edit.setAttribute('contenteditable',false);
-
-    edit.addEventListener('keyup',function() {
-
-            return self.onkeyUp()
-
-        },false
-    );
-
+    edit.setAttribute('contenteditable', true);
+    edit.addEventListener('keyup', function() { return self.onKeyUp() }, false);
     note.appendChild(edit);
     this.editField = edit;
 
     var ts = document.createElement('div');
     ts.className = 'timestamp';
-
-    ts.addEventListener('mouseDown',function(e) {
-
-            return self.onMouseDown(e)
-
-        },false
-    );
-
+    ts.addEventListener('mousedown', function(e) { return self.onMouseDown(e) }, false);
     note.appendChild(ts);
     this.lastModified = ts;
 
     document.body.appendChild(note);
-    return this
-
+    return this;
 }
 
 Note.prototype = {
-    get id() {
-        if(!('_id' in this))
-        this._id = 0;
+    get id(){
+        if (!("_id" in this))
+            this._id = 0;
         return this._id;
-
     },
+
     set id(x){
         this._id = x;
     },
 
-    get text() {
+    get text(){
         return this.editField.innerHTML;
-
     },
 
-    set text(x) {
+    set text(x){
         this.editField.innerHTML = x;
-
     },
 
-    get timestamp() {
-        if(!('_timestamp' in this))
+    get timestamp(){
+        if (!("_timestamp" in this))
             this._timestamp = 0;
         return this._timestamp;
     },
 
-    set timestamp(x) {
-        if(this._timestamp == x) {
+    set timestamp(x){
+        if (this._timestamp == x)
             return;
-        }
 
         this._timestamp = x;
         var date = new Date();
@@ -117,139 +79,175 @@ Note.prototype = {
         this.lastModified.textContent = modifiedString(date);
     },
 
-    get left() {
+    get left(){
         return this.note.style.left;
     },
 
-    set lext(x) {
+    set left(x){
         this.note.style.left = x;
-
     },
 
-    get top() {
+    get top(){
         return this.note.style.top;
     },
 
-    set top(x) {
+    set top(x){
         this.note.style.top = x;
-
     },
 
-    get zIndex() {
+    get zIndex(){
         return this.note.style.zIndex;
     },
 
-    set zIndex(x) {
+    set zIndex(x){
         this.note.style.zIndex = x;
-
     },
 
-    close: function(e) {
+    close: function(event){
         this.cancelPendingSave();
         var note = this;
-        db.transaction(function(tx) {
-            tx.executeSql('DELETE FROM MySickys WHERE id = ?', [note.id]);
-
+        db.transaction(function(tx){
+            tx.executeSql("DELETE FROM MyStickys WHERE id = ?", [note.id]);
         });
-
         document.body.removeChild(this.note);
-
     },
 
-    saveSoon: function() {
+    saveSoon: function(){
         this.cancelPendingSave();
         var self = this;
-        this._saveTimer = setTimeout(function(){
-
-            self.save();
-        },200);
+        this._saveTimer = setTimeout(function() { self.save() }, 200);
     },
 
-    cancelPendingSave: function() {
-        if(!('_saveTimer' in this)) {
+    cancelPendingSave: function(){
+        if (!("_saveTimer" in this))
             return;
-        }
-
         clearTimeout(this._saveTimer);
         delete this._saveTimer;
     },
-        save: function() {
-            this.cancelPendingSave();
-            if('dirty' in this) {
-                this.timestamp = new Date().getTime();
-                delete this.dirty;
+
+    save: function(){
+        this.cancelPendingSave();
+
+        if ("dirty" in this) {
+            this.timestamp = new Date().getTime();
+            delete this.dirty;
+        }
+
+        var note = this;
+        db.transaction(function (tx)
+        {
+            tx.executeSql("UPDATE MyStickys SET note = ?, timestamp = ?, left = ?, top = ?, zindex = ? WHERE id = ?", [note.text, note.timestamp, note.left, note.top, note.zIndex, note.id]);
+        });
+    },
+
+    saveAsNew: function(){
+        this.timestamp = new Date().getTime();
+
+        var note = this;
+        db.transaction(function (tx) {
+            tx.executeSql("INSERT INTO MyStickys (id, note, timestamp, left, top, zindex) VALUES (?, ?, ?, ?, ?, ?)", [note.id, note.text, note.timestamp, note.left, note.top, note.zIndex]);
+        });
+    },
+
+    onMouseDown: function(e){
+        captured = this;
+        this.startX = e.clientX - this.note.offsetLeft;
+        this.startY = e.clientY - this.note.offsetTop;
+        this.zIndex = ++highestZ;
+
+        var self = this;
+        if (!("mouseMoveHandler" in this)) {
+            this.mouseMoveHandler = function(e) { return self.onMouseMove(e) }
+            this.mouseUpHandler = function(e) { return self.onMouseUp(e) }
+        }
+
+        document.addEventListener('mousemove', this.mouseMoveHandler, true);
+        document.addEventListener('mouseup', this.mouseUpHandler, true);
+
+        return false;
+    },
+
+    onMouseMove: function(e){
+        if (this != captured)
+            return true;
+
+        this.left = e.clientX - this.startX + 'px';
+        this.top = e.clientY - this.startY + 'px';
+        return false;
+    },
+
+    onMouseUp: function(e){
+        document.removeEventListener('mousemove', this.mouseMoveHandler, true);
+        document.removeEventListener('mouseup', this.mouseUpHandler, true);
+
+        this.save();
+        return false;
+    },
+
+    onNoteClick: function(e){
+        this.editField.focus();
+        getSelection().collapseToEnd();
+    },
+
+    onKeyUp: function(){
+        this.dirty = true;
+        this.saveSoon();
+    },
+}
+
+function loaded(){
+    db.transaction(function(tx) {
+        tx.executeSql("SELECT COUNT(*) FROM MyStickys", [], function(result) {
+            loadNotes();
+        }, function(tx, error) {
+            tx.executeSql("CREATE TABLE MyStickys (id REAL UNIQUE, note TEXT, timestamp REAL, left TEXT, top TEXT, zindex REAL)", [], function(result) {
+                loadNotes();
+            });
+        });
+    });
+}
+
+function loadNotes(){
+    db.transaction(function(tx) {
+        tx.executeSql("SELECT id, note, timestamp, left, top, zindex FROM MyStickys", [], function(tx, result) {
+            for (var i = 0; i < result.rows.length; ++i) {
+                var row = result.rows.item(i);
+                var note = new Note();
+                note.id = row['id'];
+                note.text = row['note'];
+                note.timestamp = row['timestamp'];
+                note.left = row['left'];
+                note.top = row['top'];
+                note.zIndex = row['zindex'];
+
+                if (row['id'] > highestId)
+                    highestId = row['id'];
+                if (row['zindex'] > highestZ)
+                    highestZ = row['zindex'];
             }
 
-            var note this;
-            db.transaction(function(tx) {
-
-                tx.executeSql('Update MyStickys SET note = ?,TIMESTAMP = ?, left = ?, top = ?, zIndex = ? WHERE id = ?',[note.text,note.timestamp,note.left,note.top, note.zIndex,note.id]);
-            });
-        },
-
-        saveAsNew: function() {
-            this.timestamp = new Date.getTime();
-
-            var note = this;
-            db.transaction(function(tx) {
-                tx.executeSql("INSERT INTO Mystickys (id, note, timestamp, left, top, zindex) VALUES (?, ?, ?, ?, ?, ?)", [note.id, note.text, note.timestamp, note.left, note.top, note.zindex]);
-
-
-            });
-        },
-
-            onMouseDown: function(e) {
-
-        capture = this;
-        this.startX = e.clientX - this.note.offsetLeft;
-        this.startY = e.clientY - this.note.offsetRight;
-        this.zindex = ++highestZ;
-
-    var self = this;
-
-    if(!('mouseMoveHandler' in this)) {
-        this.mouseMoveHandler = function(e) {return self.onMouseMove (e)}
-        this.upMouseHandler = function(e) {return self.onMouseUp(e)}
-    }
-
-    document.addEventListener('mousemove', this.mousemovehandler, true);
-    document.addEventListener('mouseup', this.mouseUphandler, true);
-
-},
-
-onMouseMove: function(e) {}
-
-if(this != captured) {
-    return true;
+            if (!result.rows.length)
+                newNote();
+        }, function(tx, error) {
+            alert('Failed to retrieve notes from database - ' + error.message);
+            return;
+        });
+    });
 }
 
-this.left = e.clientX - this.startX + 'px';
-this.top = e.clientY - this.startY + 'px';
-return false;
-
+function modifiedString(date){
+    return 'Note Last Modified: ' + date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
 }
 
-onMouseUp: function(e) {
-    document.removeEventListener('mousemove',this.mouseOverHandler, true);
-    document.removeEventListener('mouseUp',this.mouseUpHandler, true);
-
-    this.save();
-    return false;
+function newNote(){
+    var note = new Note();
+    note.id = ++highestId;
+    note.timestamp = new Date().getTime();
+    note.left = Math.round(Math.random() * 400) + 'px';
+    note.top = Math.round(Math.random() * 500) + 'px';
+    note.zIndex = ++highestZ;
+    note.saveAsNew();
 }
 
-onNoteClick: function(e) {
-
-    this.editField.focus();
-    getSelection().collapseToEnd();
-
-},
-
-onKeyUp() {
-    this.dirty = true;
-    this.saveSoon();
-
-
-
-    }
-
-}
+if (db != null)
+    addEventListener('load', loaded, false);
