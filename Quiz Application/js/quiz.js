b7a3ea53e33545c90481bdfb362d445ab39a71b3
raitraak <raitraak@khk.ee>
2016-01-07 13:32:09 +0200

    Lecture 47: Process Questions [Delivers #111215244]

--- Quiz Application/js/quiz.js
+++ Quiz Application/js/quiz.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
     $('#q1 #submit').click(function() {
 
         $('.questionForm').hide();
+        process('q1');
         $('#q2').fadeIn(300);
         return false;
     });
@@ -31,6 +32,7 @@ $(document).ready(function() {
     $('#q2 #submit').click(function() {
 
         $('.questionForm').hide();
+        process('q2');
         $('#q3').fadeIn(300);
         return false;
     });
@@ -38,6 +40,7 @@ $(document).ready(function() {
     $('#q3 #submit').click(function() {
 
         $('.questionForm').hide();
+        process('q3');
         $('#q4').fadeIn(300);
         return false;
     });
@@ -45,6 +48,7 @@ $(document).ready(function() {
     $('#q4 #submit').click(function() {
 
         $('.questionForm').hide();
+        process('q4');
         $('#q5').fadeIn(300);
         return false;
     });
@@ -52,12 +56,57 @@ $(document).ready(function() {
     $('#q5 #submit').click(function() {
 
         $('.questionForm').hide();
+        process('q5');
         $('#results').fadeIn(300);
         return false;
     });
 
 
 });
+
+//Process ANW
+
+function process(q) {
+    if(q=='q1') {
+        var submitted = $('input[name=q1]:checked').val();
+        if(submitted==sessionStorage.a1) {
+            score++;
+        }
+    }
+
+    if(q=='q2') {
+        var submitted = $('input[name=q2]:checked').val();
+        if(submitted==sessionStorage.a2) {
+            score++;
+        }
+    }
+
+    if(q=='q3') {
+        var submitted = $('input[name=q3]:checked').val();
+        if(submitted==sessionStorage.a3) {
+            score++;
+        }
+    }
+
+    if(q=='q4') {
+        var submitted = $('input[name=q4]:checked').val();
+        if(submitted==sessionStorage.a4) {
+            score++;
+        }
+    }
+
+    if(q=='q5') {
+        var submitted = $('input[name=q5]:checked').val();
+        if(submitted==sessionStorage.a5) {
+            score++;
+        }
+
+        $('#results').html('<h3>Your Final Score Is: '+score+' out of 5</h3><a href="index.html">Take Quiz Again</a>');
+    }
+    return false;
+}
+
+
 
 //Add event listener
 
