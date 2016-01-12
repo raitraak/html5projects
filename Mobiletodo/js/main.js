6aec5f807de2c99b353fc4b92d06b70c0af56a7c
raitraak <raitraak@khk.ee>
2016-01-12 19:24:42 +0200

    Lecture 65: Updatingtodos [Delivers #111535382]

--- Mobiletodo/js/main.js
+++ Mobiletodo/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     if(localStorage.getItem('todos') != null){
         //Loop through and output li items
         $.each(todoList,function(key, value){
-            $('#todos').prepend('<li id="task-'+i+'"><a id="todo_link" href="#edit" data-todo_name ="'+value.todo_name+'" data-todo_date="'+value.todo_date+'">'+value.todo_name+'</a></li>');
+            $('#todos').prepend('<li id="task-'+i+'"><a id="todo_link" href="#edit" data-todo_name ="'+value.todo_name+'" data-todo_date="'+value.todo_date+'">'+value.todo_name+' <span>'+value.todo_date+'</span></a></li>');
             i++;
         });
         //Refresh
@@ -42,6 +42,34 @@ $(document).ready(function(){
         }
     });
 
+    //Edit Todo
+    $('#edit_form').submit(function(){
+        currentTodoName = localStorage.getItem('currentTodoName');
+        currentTodoDate = localStorage.getItem('currentTodoDate');
+        //Loop through todos
+        for(var i=0; i < todoList.length; i++){
+            if(todoList[i].todo_name == currentTodoName){
+                todoList.splice(i,1);
+            }
+            localStorage.setItem('todos',JSON.stringify(todoList));
+        }
+
+        //Create a new todo
+        var todo_name_edit = $('#todo_name_edit').val();
+        var todo_date_edit = $('#todo_date_edit').val();
+
+        var todos = JSON.parse(localStorage.getItem('todos'));
+
+        //Create array with new values
+        var update_todo = {
+            "todo_name": todo_name_edit,
+            "todo_date": todo_date_edit
+        };
+
+        todos.push(update_todo);
+        localStorage.setItem('todos',JSON.stringify(todos));
+    });
+
     //Delete Todo
     $('#edit_form').on('click','#delete',function(){
         currentTodoName = localStorage.getItem('currentTodoName');
@@ -62,11 +90,12 @@ $(document).ready(function(){
         localStorage.setItem('currentTodoDate',$(this).data('todo_date'));
     });
 
+    //Insert current data into edit form
     $(document).on('pageshow','#edit',function(){
         currentTodoName = localStorage.getItem('currentTodoName');
         currentTodoDate = localStorage.getItem('currentTodoDate');
-        $('#edit_form input[name=todo_name]',this).val(currentTodoName);
-        $('#edit_form input[name=todo_date]',this).val(currentTodoDate);
+        $('#edit_form input[name=todo_name_edit]',this).val(currentTodoName);
+        $('#edit_form input[name=todo_date_edit]',this).val(currentTodoDate);
     });
 
     $(document).on('pageshow','#home',function(){
