37d09b0a328c8434bafae333330d1ee22b977e10
raitraak <raitraak@khk.ee>
2016-01-12 13:19:35 +0200

    Lecture 55: Volume and duration [Delivers #111506564]

--- HTML5 Audio Player/js/main.js
+++ HTML5 Audio Player/js/main.js
@@ -21,3 +21,26 @@ $('#stop').click(function() {
     audio.currentTime = 0;
 
 });
+
+//Volume control
+$('#volume').change(function(){
+    audio.volume = parseFloat(this.value / 10);
+});
+
+//Time/Duration
+function showDuration(){
+    $(audio).bind('timeupdate',function(){
+        //Get hours and minutes
+        var s = parseInt(audio.currentTime % 60);
+        var m = parseInt(audio.currentTime / 60) % 60;
+        if(s < 10){
+            s = '0'+s;
+        }
+        $('#duration').html(m + ':'+ s);
+        var value = 0;
+        if(audio.currentTime > 0){
+            value = Math.floor((100 / audio.duration) * audio.currentTime);
+        }
+        $('#progress').css('width',value+'%');
+    });
+}
